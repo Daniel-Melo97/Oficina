2510a108af8e47b29c446f830fc356af3e74949c
Daniel-Melo97 <daniellemosmelo@hotmail.com>
2021-04-26 11:28:40 -0300

    Adicionando bootstrap

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,7 +1,16 @@
+import {Button} from 'react-bootstrap';
+
 
 export default function Main(){
    
     return(
-       <h1>Hello Worddld</h1>
+        <div>
+            <h1>Hello Worddld</h1>
+            <Button className="btn-success">
+                clique aqui
+            </Button>
+
+        </div>
+       
     );
 }
